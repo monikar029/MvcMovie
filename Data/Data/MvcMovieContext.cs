d53da16c8d7482370891ae54967ea03c17a0af91
Monika <monikar029@gmail.com>
2021-05-29 22:59:57 +0530

    Initial Commit

---
+++ Data/Data/MvcMovieContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+using System.Linq;
+
+namespace MvcMovie.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(t => t.GetProperties())
+                .Where(p => p.ClrType == typeof(decimal) || p.ClrType == typeof(decimal?)))
+            {
+                property.SetColumnType("decimal(18, 2)");
+            }
+
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
